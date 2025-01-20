7b883632954aeed00af37d776b11e22aa8ffd138
dusaan-m <dusanmilivojevic01@gmail.com>
2025-01-20 22:07:15 +0100

    Add city configuration classes for bus API integration
    Introduced `CityConfig` and its subclasses to define configurations for Beograd, Novi Sad, and Nis. These configurations include API details and AES encryption keys for secure communication with respective endpoints. This change sets up the structure for handling city-specific data more efficiently.

---
+++ src/srbusapi/config.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CityConfig:
+    name: str
+    url: str
+    api_key: str
+    stations_endpoint: str
+    api_endpoint: str
+
+
+@dataclass
+class AES:
+    key: bytes
+    iv: bytes
+
+
+@dataclass
+class BeogradConfig(CityConfig):
+    name = "Beograd"  #: Obicno ime
+    url = "https://announcement-bgnaplata.ticketing.rs"
+    api_key = "1688dc355af72ef09287"
+    stations_endpoint = "/publicapi/v1/networkextended.php"
+    api_endpoint = "/publicapi/v2/api.php"
+
+    # AES encryption keys and ivs
+    aes_arrivals = AES(
+        key=b"3+Lhz8XaOli6bHIoYPGuq9Y8SZxEjX6eN7AFPZuLCLs=",
+        iv=b"IvUScqUudyxBTBU9ZCyjow==",
+    )
+    aes_route = AES(
+        key=b"LLUdzKaaLqm2QAyVryeBlAbUaX/1uDJxZAmgPZ2N2r8=",
+        iv=b"6GWe4+QzF6NDLS9kRef76A==",
+    )
+    aes_route_version = AES(
+        key=b"0cQXGKcGHSVEcTOt+1UDBzZ5XpzKB+Juz4C6OEnoVwo=",
+        iv=b"VsuUn8cH9GWaZshcoWOjbw==",
+    )
+    aes_line_number = AES(
+        key=b"hOKZ6e2ZmzjDWjagmhhmTWme94ao31AlHQ+msJnDS4Q=",
+        iv=b"YW62pldkwSXGqoWcclKPeA==",
+    )
+
+
+class NoviSadConfig(CityConfig):
+    name = "Novi sad"
+    url = "https://online.nsmart.rs"
+    api_key = "4670f468049bbee2260"
+    stations_endpoint = "/publicapi/v1/networkextended.php"
+    api_endpoint = "/publicapi/v1/announcement/announcement.php"
+
+
+class NisConfig(CityConfig):
+    name = "Nis"
+    url = "https://online.jgpnis.rs"
+    api_key = "1688dc355af72ef09287"
+    stations_endpoint = "/publicapi/v1/networkextended.php"
+    api_endpoint = "/publicapi/v1/announcement/announcement.php"
