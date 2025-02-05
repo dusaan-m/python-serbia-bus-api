c12fd2dc5d69e5507de430acbf795679749252ac
dusaan-m <dusanmilivojevic01@gmail.com>
2025-02-05 14:45:22 +0100

    Add error handling to encryption and decryption functions
    Introduced custom exceptions to handle errors in encrypt and decrypt functions. This ensures clearer error messaging for invalid inputs or failures during encryption and decryption processes. Enhanced code robustness by validating input types and catching potential decoding and padding errors.

--- src/srbusapi/crypto.py
+++ src/srbusapi/crypto.py
@@ -4,25 +4,35 @@ from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
+from srbusapi.exceptions import EncryptionError, DecryptionError
+
 
 def encrypt(base_dict: dict, b64_key: bytes, b64_iv: bytes) -> bytes:
-    key = b64decode(b64_key)
-    iv = b64decode(b64_iv)
+    try:
+        if not isinstance(base_dict, dict):
+            raise EncryptionError("Input data must be a dictionary")
+        key = b64decode(b64_key)
+        iv = b64decode(b64_iv)
 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    padded_data = pad(json.dumps(base_dict).encode("utf-8"), AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        padded_data = pad(json.dumps(base_dict).encode("utf-8"), AES.block_size)
 
-    encrypted_data = cipher.encrypt(padded_data)
-    base64_encoded = b64encode(encrypted_data)
+        encrypted_data = cipher.encrypt(padded_data)
+        base64_encoded = b64encode(encrypted_data)
 
-    return base64_encoded
+        return base64_encoded
+    except (ValueError, TypeError) as e:
+        raise EncryptionError(f"Encryption failed: {str(e)}") from e
 
 
 def decrypt(cipher_text: bytes, b64_key: bytes, b64_iv: bytes) -> dict:
-    key = b64decode(b64_key)
-    iv = b64decode(b64_iv)
+    try:
+        key = b64decode(b64_key)
+        iv = b64decode(b64_iv)
 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted = unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size)
 
-    return json.loads(decrypted.decode("utf-8"))
+        return json.loads(decrypted.decode("utf-8"))
+    except (ValueError, TypeError, json.JSONDecodeError) as e:
+        raise DecryptionError(f"Decryption failed: {str(e)}") from e
