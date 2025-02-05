cf21928e8c5c270c076a65e4ca875b979e0b0e4d
dusaan-m <dusanmilivojevic01@gmail.com>
2025-02-05 14:46:24 +0100

    Refactor city config classes to use BaseCityConfig.
    Replaced `CityConfig` with `BaseCityConfig` to streamline inheritance and structure. Added `@dataclass(frozen=True, slots=True)` for city-specific config classes to improve immutability and memory efficiency.

--- src/srbusapi/config.py
+++ src/srbusapi/config.py
@@ -1,15 +1,12 @@
 from dataclasses import dataclass
-from typing import Optional
 
 
-@dataclass
-class CityConfig:
+class BaseCityConfig:
     name: str
     url: str
     api_key: str
     stations_endpoint: str
     api_endpoint: str
-    aes: Optional["AES"] = None
 
 
 @dataclass
@@ -18,7 +15,8 @@ class AES:
     iv: bytes
 
 
-class BeogradConfig(CityConfig):
+@dataclass(frozen=True, slots=True)
+class BeogradConfigBase(BaseCityConfig):
     name = "Beograd"  #: Obicno ime
     url = "https://announcement-bgnaplata.ticketing.rs"
     api_key = "1688dc355af72ef09287"
@@ -44,15 +42,17 @@ class BeogradConfig(CityConfig):
     )
 
 
-class NoviSadConfig(CityConfig):
-    name = "Novi sad"
+@dataclass(frozen=True, slots=True)
+class NoviSadConfigBase(BaseCityConfig):
+    name = "Novi_Sad"
     url = "https://online.nsmart.rs"
     api_key = "4670f468049bbee2260"
     stations_endpoint = "/publicapi/v1/networkextended.php"
     api_endpoint = "/publicapi/v1/announcement/announcement.php"
 
 
-class NisConfig(CityConfig):
+@dataclass(frozen=True, slots=True)
+class NisConfigBase(BaseCityConfig):
     name = "Nis"
     url = "https://online.jgpnis.rs"
     api_key = "1688dc355af72ef09287"
